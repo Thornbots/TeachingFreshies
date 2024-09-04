e5e8bc1d78d1b115ff13d2abffbe56f2f8ea67b0
agmui <anthonynmui@gmail.com>
2024-09-03 21:08:06 -0400

    serrated mini project branch

--- MCB-project/src/main.cpp
+++ MCB-project/src/main.cpp
@@ -1,16 +1,8 @@
 #include "tap/communication/serial/uart_terminal_device.hpp"
 
-#include "Gimbal/GimbalSubsystem.h"
-#include "Robot.h"
-
 #include "drivers_singleton.hpp"
 
 using namespace tap::communication::serial;
-
-
-static tap::arch::PeriodicMicroTimer RunTimer(
-    10000);  // Don't ask me why. This only works as a global. #Certified Taproot Moment
-
 
 int main()
 {
@@ -24,20 +16,11 @@ int main()
     drivers->leds.init();
     bool led_state = true;
 
-    ThornBots::GimbalSubsystem* gimbalSubsystem = new ThornBots::GimbalSubsystem(drivers);
-    ThornBots::Robot* robot = new ThornBots::Robot(drivers, gimbalSubsystem, &s);
-
-    robot->initialize();
-
     while (1)
     {
-        if (RunTimer.execute())
-        {  // Calling this function every 10 us at max
-            drivers->leds.set(tap::gpio::Leds::Green, led_state);
-            led_state = !led_state;
-
-            robot->update();
-        }
-        drivers->canRxHandler.pollCanData();  // checks to see if a msg is waiting
+        drivers->leds.set(tap::gpio::Leds::Green, led_state);
+        led_state = !led_state;
+        s.printf("hello world\n");
+        modm::delay_ms(10);
     }
 }
